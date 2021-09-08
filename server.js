e7a95c6e8ffd75903c6ee99fdb1477b509801e67
Rethaniel Ramos <rethanielramos@gmail.com>
2021-09-08 17:38:22 +0800

    add Procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const { Server } = require("socket.io");
 const io = new Server(server);
 
